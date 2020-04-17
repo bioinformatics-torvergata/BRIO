b8b109aa39153c520c1af47546ad386343114a4a
NoiSe <marco23.p@gmail.com>
2020-04-17 11:54:19 +0200

    the script for combining the motif files is ready for sequence motifs too

--- scripts/utils/combine_motif_files.py
+++ scripts/utils/combine_motif_files.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
-
+"""
+group old motif files in a more compact way
+"""
 
 import re
 import os
-
+import sys
 import itertools as it
 
 
-# In[27]:
-
+print("please remember that for now,\n\
+    eCLIP regex pattern is enforced on human. Remember this if\n\
+    new mouse eCLIP data comes along")
 
 hg19_re = re.compile("^.*_hg19_.*$")
 mm9_re = re.compile("^.*_mm9_.*$")
@@ -32,50 +34,35 @@ d_species = {
 }
 
 
-# In[28]:
-
-
-motifFolder = "../BRIO/some_motifs_str/"
-
-
-# In[37]:
-
-
-
+if len(sys.argv)<3:
+    print("please specify the directory where the motifs are stored (1) and one between {seq|str}")
+    exit()
+#motifFolder = "../BRIO/some_motifs_str/"
+motifFolder = sys.argv[1]
+seqFlag = (sys.argv[2] == 'seq')
+type_ID = "#NT" if seqFlag else "#BEAR"
 
 for filters in it.product(d_tech, d_species):
     print(filters)
-    o = open("motifs_{}_{}".format(filters[0], filters[1]), 'w')
+    o = open("motifs_{}_{}_{}.txt".format(filters[0], filters[1], sys.argv[2]), 'w')
     for file in os.listdir(motifFolder):
         
         if filters[0] == 'eCLIP':
-            if d_tech[filters[0]].match(file):
+            """eCLIP has weird name patterns, only for human now"""
+            if d_tech[filters[0]].match(file) and filters[1] == 'hg19':
                 fpath = os.path.join(motifFolder, file)
                 o.write("\n\n#NAME\n")
                 o.write(file + "\n\n")
-                o.write("#BEAR\n")
+                o.write(f"{type_ID}\n") 
                 o.write(open(fpath).read())   
         else:        
             if d_tech[filters[0]].match(file) and d_species[filters[1]].match(file):
                 fpath = os.path.join(motifFolder, file)
                 o.write("\n\n#NAME\n")
                 o.write(file + "\n\n")
-                o.write("#BEAR\n")
+                o.write(f"{type_ID}\n")
                 o.write(open(fpath).read())   
         
 
         
     o.close()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
