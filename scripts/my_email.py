976ee5c7c74134f1a1fd904d0c297db837d5b6a1
AndreaGuarracino <andreaguarracino@outlook.com>
2020-12-19 18:40:10 +0100

    removed tag <a> and typos

--- scripts/my_email.py
+++ scripts/my_email.py
@@ -3,10 +3,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def send_email_with_code(user_email, code):
-    link_results = f'http://160.80.35.91:8080//waiting?uid={code}'
+    link_results = f'http://160.80.35.91:8080/waiting?uid={code}'
 
     mail_content = "Thanks for choosing BRIO,\nyou can find your results using the following identifier:\n\n" + \
-                   f"<a href={link_results}>" + link_results + "</a>\n\n\nBest regards,\nBRIO Team"
+                   link_results + "\n\n\nBest regards,\nBRIO Team"
 
     # The mail addresses and password
     sender_address = 'email@email.com'
